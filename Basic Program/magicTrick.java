577d98ba42a6acf24906b27996e23e0776aa89e8
arohanraj <90061844+arohanraj@users.noreply.github.com>
2022-04-12 02:42:42 +0530

    Add files via upload

---
+++ Basic Program/magicTrick.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class magicTrick {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int res = (2*a + b)/2;
+        System.out.println(res);
+    }    
+}
